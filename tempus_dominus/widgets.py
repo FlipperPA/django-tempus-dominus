3d5805dd31fc4324fb59ecef423353cac776e1f2
Allen, Timothy <tallen@wharton.upenn.edu>
2018-04-12 19:37:16 +0000

    WIP: it works initially.

--- tempus_dominus/widgets.py
+++ tempus_dominus/widgets.py
@@ -20,7 +20,7 @@ class TempusDominusMixin(object):
         )
 
     html_template = """
-        <input type="{type}" name="{name}"{value}{attrs} data-toggle="datetimepicker" data-target="#{picker_id}">
+        <input type="{type}" name="{name}"{value}{attrs} data-toggle="datetimepicker" data-target="#{picker_id}" id="{picker_id}">
         <script type="text/javascript">
             $(function () {{
                 $('#{picker_id}').datetimepicker({options});
@@ -29,24 +29,18 @@ class TempusDominusMixin(object):
     """
 
     def render(self, name, value, attrs=None):
+        self.attrs['class'] = 'form-control datetimepicker-input'
+
         from pprint import pprint
         print('SELF')
         pprint(dir(self))
-        print('ATTRS')
-        pprint(attrs)
-        # print('VALUE', self.subwidgets())
 
         context = super().get_context(name, value, attrs)
-        print('CONTEXT')
-        pprint(context)
-
-        """
-        {% if widget.value != None %} value="{{ widget.value|stringformat:'s' }}"{% endif %}
-        % include "django/forms/widgets/attrs.html" %
-        """
 
         attr_html = ''
         for attr_key, attr_value in self.attrs.items():
+            if attr_key == "class":
+                print("CLASS", attr_value)
             attr_html += ' {key}="{value}"'.format(
                 key=attr_key,
                 value=attr_value,
@@ -55,6 +49,7 @@ class TempusDominusMixin(object):
         value_html = ''
         if context['widget']['value'] is not None:
             value_html = ' value="{}"'.format(context['widget']['value'])
+            self.options['date'] = context['widget']['value']
 
         field_html = self.html_template.format(
             type=context['widget']['type'],
@@ -71,57 +66,14 @@ class TempusDominusMixin(object):
 
 class DatePicker(TempusDominusMixin, DateInput):
     options = {
-        'format': 'L',
+        'format': 'YYYY-MM-DD',
     }
 
 
-class DateTimePicker(DateTimeInput, TempusDominusMixin):
-
-    def __init__(self, attrs=None, format=None, options=None, div_attrs=None, icon_attrs=None):
-        if not icon_attrs:
-            icon_attrs = {'class': 'fa fa-calendar'}
-        if not div_attrs:
-            div_attrs = {'class': 'input-group date'}
-        if format is None and options and options.get('format'):
-            format = self.conv_datetime_format_js2py(options.get('format'))
-        super().__init__(attrs, format)
-        if 'class' not in self.attrs:
-            self.attrs['class'] = 'form-control'
-        self.div_attrs = div_attrs and div_attrs.copy() or {}
-        self.icon_attrs = icon_attrs and icon_attrs.copy() or {}
-        self.picker_id = self.div_attrs.get('id') or None
-        if options is False:  # datepicker will not be initalized when options is False
-            self.options = False
-        else:
-            self.options = options and options.copy() or {}
-            if format and not self.options.get('format') and not self.attrs.get('date-format'):
-                self.options['format'] = self.conv_datetime_format_py2js(format)
-
-    def render(self, name, value, attrs=None):
-        if value is None:
-            value = ''
-        extra_attrs = dict()
-        extra_attrs['type'] = self.input_type
-        extra_attrs['name'] = name
-        input_attrs = self.build_attrs(attrs, extra_attrs)
-        if value != '':
-            # Only add the 'value' attribute if a value is non-empty.
-            input_attrs['value'] = force_text(self._format_value(value))
-        input_attrs = {key: conditional_escape(val) for key, val in input_attrs.items()}
-        if not self.picker_id:
-            self.picker_id = (input_attrs.get('id', '') + '_pickers').replace(' ', '_')
-        self.div_attrs['id'] = self.picker_id
-        picker_id = conditional_escape(self.picker_id)
-        div_attrs = {key: conditional_escape(val) for key, val in self.div_attrs.items()}
-        icon_attrs = {key: conditional_escape(val) for key, val in self.icon_attrs.items()}
-        html = self.html_template % dict(div_attrs=flatatt(div_attrs),
-                                         input_attrs=flatatt(input_attrs),
-                                         icon_attrs=flatatt(icon_attrs))
-        if self.options:
-            js = self.js_template % dict(picker_id=picker_id, options=json_dumps(self.options or {}))
-        else:
-            js = ''
-        return mark_safe(force_text(html + js))
+class DateTimePicker(TempusDominusMixin, DateTimeInput):
+    options = {
+        'format': 'YYYY-MM-DD HH:mm:ss',
+    }
 
 
 class TimePicker(TimeInput, TempusDominusMixin):
