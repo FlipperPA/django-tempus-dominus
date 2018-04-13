7375cb289d6157b05f64c380acf127dd3cd15114
Allen, Timothy <tallen@wharton.upenn.edu>
2018-04-13 14:23:49 +0000

    Finish up classifiers.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
