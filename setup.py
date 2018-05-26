adb3ae0c7d767774ae1508d2b2475f058e48aabf
Allen, Timothy <tallen@wharton.upenn.edu>
2018-05-26 20:02:17 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='django-tempus-dominus',
-    version="0.1.2",
+    version="0.1.3",
     description='A Django widget for the Tempus Dominus Bootstrap 4 DateTime picker.',
     long_description=long_description,
     long_description_content_type='text/markdown',
