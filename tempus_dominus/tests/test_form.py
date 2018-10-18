4dc76ac1bdc3ea336831c88b77dedf68ec263a12
Allen, Timothy <tallen@wharton.upenn.edu>
2018-10-18 19:32:34 +0000

    Work for putting in an initial test at sprints.

--- tempus_dominus/tests/test_form.py
+++ tempus_dominus/tests/test_form.py
@@ -1,8 +1,17 @@
-# content of test_sample.py
-def inc(x):
-    return x + 1
+from .forms import DateFieldForm, TimeFieldForm, DateTimeFieldForm
 
+
+def test_date_field_form():
+    """
+    Test to see ...
+    """
+    form = DateFieldForm()
+    from IPython import embed
+    embed()
+
+'''
 
 def test_answer():
     assert inc(3) == 5
     assert inc(6) == 7
+'''
