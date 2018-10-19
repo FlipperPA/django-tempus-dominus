06ce351b5e85d8093e76cb8178df63103cf20354
Bryan Collazo <bcollazo2010@gmail.com>
2018-10-19 09:56:02 -0700

    Add Widget .render tests

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -42,3 +42,20 @@ def test_datetimepicker_format_nonlocalized(settings):
 def test_get_js_format_error():
     with pytest.raises(NotImplementedError):
         widgets.TempusDominusMixin().get_js_format()
+
+
+def test_widget_render(settings):
+    settings.TEMPUS_DOMINUS_LOCALIZE = False
+    widget = widgets.DatePicker()
+    output = widget.render('inputname', '2018-01-01', attrs={'id': 'myid'})
+    input_element = '<input type="text" name="inputname" data-toggle="datetimepicker" data-target="#myid" id="myid">'
+    jquery_call = "$('#myid').datetimepicker({'format': 'YYYY-MM-DD', 'defaultDate': '2018-01-01T00:00:00'});"
+    assert input_element in output
+    assert jquery_call in output
+
+
+def test_uparsable_date_doesnt_include_default_value(settings):
+    settings.TEMPUS_DOMINUS_LOCALIZE = False
+    widget = widgets.DateTimePicker()
+    output = widget.render('inputname', 'invalid-date', attrs={'id': 'myid'})
+    assert 'defaultDate' not in output
